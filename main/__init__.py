5bb78e562156908e040e98f085a17940a9783325
raisulru <raisulru10@gmail.com>
2020-01-13 18:27:21 +0600

    Fixed db table create error

--- main/__init__.py
+++ main/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 '''from flask_mongoengine import MongoEngine
 from flask_sqlalchemy import SQLAlchemy
 from elasticsearch import Elasticsearch'''
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 from .celery import make_celery
-
 '''db = MongoEngine()
 db = SQLAlchemy()
 es = Elasticsearch(host=Config.ELASTICSEARCH_HOST)'''
 
+db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -17,10 +18,14 @@ def create_app(config_class=Config):
     ''' 
     db.init_app(app)
     '''
+    db.init_app(app)
+
     # for relational database you need to create all db first
-    '''with app.app_context():
+    with app.app_context():
+        # Import any one of your model name here
+        from main.models.test_db import User
         db.create_all()
-    '''
+    
 
 
     from .api import blue_print
--- main/models/test_db.py
+++ main/models/test_db.py
@@ -1,10 +1,10 @@
-'''from main import db
-'''
+from main import db
+
 # For Relational database when use SqlAlchemy
-'''class User(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)'''
+    email = db.Column(db.String, unique=True, nullable=False)
 
 # For NoSQL database when use flask-mongoengine
 '''class User(db.Document):
